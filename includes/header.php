1de3f0afe9457e137ef4e8cf946f5ab931c9c2d1
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-03-25 09:40:52 +0300

    Created a reusable header with bootstrap (Global UI

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Exam System</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <span class="navbar-brand">Online Exam System</span>
+    <a href="../auth/logout.php" class="btn btn-danger">Logout</a>
+  </div>
+</nav>
+<div class="container mt-4">
