4a68a91ab477f3e1ed60788c4c989968320a1643
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-03-25 12:49:37 +0300

    added modern UI feature in dashboard.php file

--- instructor/dashboard.php
+++ instructor/dashboard.php
@@ -1,8 +1,37 @@
-<?php
-session_start();
-if ($_SESSION['role'] != 'instructor') {
+<?php 
+include '../includes/header.php'; 
+
+// Security check
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'instructor') {
     header("Location: ../auth/login.php");
+    exit();
 }
 ?>
-<h1>Instructor Dashboard</h1>
-<a href="create_test.php">Create Test</a>
+
+<h2 class="mb-4">Instructor Dashboard</h2>
+
+<div class="row">
+  <div class="col-md-4">
+    <div class="card shadow p-3">
+      <h5>Create Test</h5>
+      <a href="create_test.php" class="btn btn-primary">Go</a>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card shadow p-3">
+      <h5>Add Questions</h5>
+      <a href="add_question.php" class="btn btn-success">Go</a>
+    </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="card shadow p-3">
+      <h5>View Results</h5>
+      <a href="view_results.php" class="btn btn-warning">Go</a>
+    </div>
+  </div>
+</div>
+
+<?php include '../includes/footer.php'; ?>
+
