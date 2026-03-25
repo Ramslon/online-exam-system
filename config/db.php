fe7c6df61a005849ddb6e3e3e422487dcddd0b4e
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-03-25 09:25:27 +0300

    fixed configuration error in db.php file

--- config/db.php
+++ config/db.php
@@ -1,5 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "exam_system");
+$servername = "localhost";
+$username = "root";
+$password = ""; // <-- leave empty if you didn't set a MySQL root password
+$dbname = "exam_system";
+$port = 3306;
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
